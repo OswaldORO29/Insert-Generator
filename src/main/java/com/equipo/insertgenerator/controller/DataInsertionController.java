769301593f970a649dc40dd98d54d4f6564ac05c
DanaZuazo_Personal <danalaurazuazoortiz@gmail.com>
2026-07-04 19:54:10 -0600

    anotaciones REST faltantes en DataInsertionController

--- src/main/java/com/equipo/insertgenerator/controller/DataInsertionController.java
+++ src/main/java/com/equipo/insertgenerator/controller/DataInsertionController.java
@@ -6,12 +6,13 @@ import com.equipo.insertgenerator.service.DataInsertionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@RestController
+@RequestMapping("/api/insert")
+@CrossOrigin(origins = "*")
 public class DataInsertionController {
     @Autowired
     private DataInsertionService insertionService;
